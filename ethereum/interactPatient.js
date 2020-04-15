4793dd15e547bd8b83e6509582642a01018f58ac
pratik3098 <pratik3098@gmail.com>
2020-04-14 23:51:40 -0400

    prescription interaction completed

--- ethereum/interactPatient.js
+++ ethereum/interactPatient.js
@@ -5,37 +5,61 @@ const ABI=require(path.resolve(__dirname, "./smartContracts/abi/Patient.js"))
 const Doctor=require(path.resolve(__dirname, "./smartContracts/abi/Doctor.js"))
 const Pharmacy=require(path.resolve(__dirname, "./smartContracts/abi/Pharmacy.js"))
 
-class Patient{
+module.exports=class Patient{
 
     constructor(_patientAddress){
         try{
         let provider = ethers.getDefaultProvider(config.wallet.network)
         this.contract=new ethers.Contract(_patientAddress, ABI.abi, provider)
-        this.contract.connect(new ethers.Wallet(config.wallet.privateKey, provider))
+        this.contract=this.contract.connect(new ethers.Wallet(config.wallet.privateKey, provider))
         }
         catch(err){
             console.error(err.message)
         }
     }
-// exports.initContract=async function(patientAddress){
 
-//     contract = new ethers.Contract(patientAddress, abi, provider)
-// }
 
 async addEmr(_emr, _doctorAddress){
-    let tx = await  contract.addEmr(_emr,_doctorAddress)
+    console.log(ethers.utils.hexlify(_emr))
+    let tx = await  this.contract.addEmr(ethers.utils.formatBytes32String(_emr),_doctorAddress)
     await tx.wait()
+    console.log(JSON.stringify(tx))
 }
 
-// exports.addPrescription=async function(_prescription,_doctorAddress){
-//     let tx = contract.addPrescription(_prescription,  _doctorAddress)
-//     await tx.wait()
-// }
+async addPrescription(_prescription,_doctorAddress){
+    let tx = await this.contract.addPrescription(ethers.utils.formatBytes32String(_prescription),  _doctorAddress)
+    await tx.wait()
+    console.log(tx)
+}
 
-// exports.addVerifiedEmr=async function(_emr,_doctorAddress,_sign){
-//     let tx = contract.addVerifiedEmr(_emr,_doctorAddress,_sign)
-//     await tx.wait()
-// }
+async addVerifiedEmr(_emr,_doctorAddress,_sign){
+    let tx = await this.addEmrcontract.addVerifiedEmr(ethers.utils.formatBytes32String(_emr),_doctorAddress,_sign)
+    await tx.wait()
+    console.log(tx)
+}
+
+async isEmrVerified(_emr){
+    try{
+        let res = await  this.contract.isEmrVerified(ethers.utils.formatBytes32String(_emr))
+        console.log(res)
+        return res
+        }
+        catch(err){
+            console.error(err.message)
+        }
+}
+async isPresVerified( _prescription){
+    try{
+        let res = await  this.contractisPresVerified(ethers.utils.formatBytes32String(_prescription))
+        console.log(res)
+        return res
+        }
+        catch(err){
+            console.error(err.message)
+        }
+}
+
+
 
 async verifySender(_doctorAddress){
     try{
@@ -54,10 +78,3 @@ async verifySender(_doctorAddress){
 
 
 
-async function createPatient(){
-    let pat= new Patient("0x279CAAFf6f75d9c08Ecb29928388d9C747a6C516")
-     let res = await pat.verifySender("0x2e3AEa4b4EC043c60bA45E43b40046A3cFbc0d27")
-     console.log(res)
-}
-
-createPatient()
